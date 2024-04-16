f1990679b2743e31e874a5374b235b6deb09ca06
Dhawal Bahe <dhawalbahe1999@gmail.com>
2024-04-16 16:55:59 +0530

    code update

--- UserService/src/main/java/com/example/UserService/Controller/UserController.java
+++ UserService/src/main/java/com/example/UserService/Controller/UserController.java
@@ -1,15 +1,21 @@
 package com.example.UserService.Controller;
 
-import com.example.UserService.Entity.User;
-import com.example.UserService.Service.UserService;
-import com.example.UserService.ServiceImpl.UserSeviceImpl;
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.UserService.Entity.User;
+import com.example.UserService.Service.UserService;
 
 @RestController
 @RequestMapping("/user")
@@ -21,19 +27,46 @@ public class UserController {
 	private UserService userService;
 
 	@PostMapping("/createUser")
-	String createUser(@RequestBody User user) {
-		String existEmployee = null;
-		if (user != null) {
-			try {
+	public ResponseEntity<String> createUser(@RequestBody User user) {
+//		String existEmployee = null;
+//		if (user != null) {
+//			try {
+//				logger.info("Creating User: {}", user.getName());
+//				existEmployee = userService.createUser(user);
+//			} catch (Exception e) {
+//				logger.error(e.toString());
+//			}
+//		} else {
+//			logger.info("User is null");
+//		}
+//		return existEmployee;
+		try {
+			if (user != null) {
 				logger.info("Creating User: {}", user.getName());
-				existEmployee = userService.createUser(user);
-			} catch (Exception e) {
-				logger.error(e.toString());
+				userService.createUser(user);
+				return new ResponseEntity<String>("User Created Sucessfully!!!", HttpStatus.OK);
+			} else {
+				return new ResponseEntity<String>("User Not found!!!", HttpStatus.NOT_FOUND);
 			}
-		} else {
-			logger.info("User is null");
+		} catch (Exception e) {
+			logger.error(e.toString());
 		}
-		return existEmployee;
+		return null;
+	}
+
+	@GetMapping("/getUsersById")
+	public User getUsers(@RequestParam Long id) {
+		if (id != 0) {
+			logger.info("get user by {}", id);
+			return userService.getUserById(id);
+		} else {
+			return null;
+		}
+	}
+
+	@GetMapping("/getUsers")
+	public List<User> getAllUsers() {
+		return userService.getAllUsers();
 	}
 
 	@PostMapping("/deleteUser")
