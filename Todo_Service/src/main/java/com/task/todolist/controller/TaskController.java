3b88cb223185fca313a9a47786865c6c9f4264aa
harshit123vyas <121675255+harshit-vyas@users.noreply.github.com>
2024-04-19 14:48:20 +0530

    add try-catch

--- Todo_Service/src/main/java/com/task/todolist/controller/TaskController.java
+++ Todo_Service/src/main/java/com/task/todolist/controller/TaskController.java
@@ -31,112 +31,163 @@ public class TaskController {
 	
 	@GetMapping("/user/{userId}")
 	public List<Task> getTaskByUserId(@PathVariable Long userId){
-		if(userId != null) {
-			return taskService.getByUserId(userId);
-			
+		try {
+			if(userId != null && userId!=0) {
+				return taskService.getByUserId(userId);
+				
+			}
+		} catch (Exception e) {
+			log.info("exception : {}",e);
 		}
+		
 		return null;
 	}
 	
 	@GetMapping
 	public ResponseEntity<List<Task>> findAllTask(){
-		log.info("fetching all tasks");
-		List<Task> taskList = taskService.getAllTask();
-		log.info("taskList :{} ",taskList);
-		if(!taskList.isEmpty()) {
-			return new ResponseEntity<List<Task>>(taskList,HttpStatus.OK);
-		}
-		else {
-			return new ResponseEntity<List<Task>>(taskList,HttpStatus.NO_CONTENT);
+		try {
+			log.info("fetching all tasks");
+			List<Task> taskList = taskService.getAllTask();
+			log.info("taskList :{} ",taskList);
+			if(!taskList.isEmpty()) {
+				return new ResponseEntity<List<Task>>(taskList,HttpStatus.OK);
+			}
+			else {
+				return new ResponseEntity<List<Task>>(taskList,HttpStatus.NO_CONTENT);
+			}
+		} catch (Exception e) {
+			log.info("exception : {}",e);
 		}
 		
+		return null;
 	}
 	
 	
 	@GetMapping("/{id}")
 	public ResponseEntity<Task> getTaskById(@PathVariable Integer id){
-		log.info("get Task By Id : {}",id );
-		Task task= taskService.getTaskById(id);
-		log.info("task :{}",task);
-		if(task!=null) {
-			return new ResponseEntity<Task>(task,HttpStatus.FOUND);
+		try {
+			
+			log.info("get Task By Id : {}",id );
+			Task task= taskService.getTaskById(id);
+			log.info("task :{}",task);
+			if(task!=null) {
+				return new ResponseEntity<Task>(task,HttpStatus.FOUND);
+			}
+			else {
+				return  new ResponseEntity<Task>(task,HttpStatus.NOT_FOUND);
+			}
+			
+			
+		} catch (Exception e) {
+			log.info("exception : {}",e);
 		}
-		else {
-			return  new ResponseEntity<Task>(task,HttpStatus.NOT_FOUND);
-		}
+		return null;
 	}
 	
 	@PostMapping
 	public ResponseEntity<Task> addTask(@RequestBody Task task){
-		log.info("adding new task : {}",task);
-		Task addedTask = taskService.addTask(task);
-		if(task!=null) {
-			return new ResponseEntity<Task>(addedTask,HttpStatus.CREATED);
+		try {
+			log.info("adding new task : {}",task);
+			Task addedTask = taskService.addTask(task);
+			if(task!=null) {
+				return new ResponseEntity<Task>(addedTask,HttpStatus.CREATED);
+			}
+			else {
+				return new ResponseEntity<Task>(addedTask,HttpStatus.BAD_REQUEST);
+			}
+			
+		} catch (Exception e) {
+			log.info("exception : {}",e);
 		}
-		else {
-			return new ResponseEntity<Task>(addedTask,HttpStatus.BAD_REQUEST);
-		}
+		return null;
 	}
 	
 	@PostMapping("/update/{id}")
 	public ResponseEntity<Task> updateTask(@RequestBody Task task, @PathVariable Integer id){
-		log.info("updating the task :{}", task);
-		Task updatedTask=taskService.getTaskById(id);
-		if(updatedTask!=null) {
-			return new ResponseEntity<Task>(taskService.updateTask(task, id),HttpStatus.OK);
+		try {
+			log.info("updating the task :{}", task);
+			Task updatedTask=taskService.getTaskById(id);
+			if(updatedTask!=null) {
+				return new ResponseEntity<Task>(taskService.updateTask(task, id),HttpStatus.OK);
+			}
+			else {
+				return new ResponseEntity<Task>(task,HttpStatus.NOT_FOUND);
+			}
+			
+		} catch (Exception e) {
+			log.info("exception : {}",e);
 		}
-		else {
-			return new ResponseEntity<Task>(task,HttpStatus.NOT_FOUND);
-		}
+		return null;
 	}
 	
 	
 	@PostMapping("/delete/{id}")
 	public ResponseEntity<String> deleteTaskById(@PathVariable Integer id){
-		log.info("deleting task by id : {} ",id);
-		if(taskService.deleteTaskById(id)) {
-			return new ResponseEntity<String>("Task deleted successfully",HttpStatus.OK);
+		try {
+			log.info("deleting task by id : {} ",id);
+			if(taskService.deleteTaskById(id)) {
+				return new ResponseEntity<String>("Task deleted successfully",HttpStatus.OK);
+			}
+			else {
+				return new ResponseEntity<String>("Task not with given id not exist ",HttpStatus.NOT_FOUND);
+			}
+		} catch (Exception e) {
+			log.info("exception : {}",e);
 		}
-		else {
-			return new ResponseEntity<String>("Task not with given id not exist ",HttpStatus.NOT_FOUND);
-		}
+		return null;
 	}
 	
 	@GetMapping("/remainning")
 	public ResponseEntity<List<Task>> getRemainningTask(){
-		List<Task> taskList = taskService.getAllRemainningTask();
-		log.info("taskList :{}",taskList);
-		if(!taskList.isEmpty()) {
-			return new ResponseEntity<List<Task>>(taskList,HttpStatus.OK);
+		try {
+			List<Task> taskList = taskService.getAllRemainningTask();
+			log.info("taskList :{}",taskList);
+			if(!taskList.isEmpty()) {
+				return new ResponseEntity<List<Task>>(taskList,HttpStatus.OK);
+			}
+			else {
+				return new ResponseEntity<List<Task>>(taskList,HttpStatus.NO_CONTENT);
+			}
+			
+		} catch (Exception e) {
+			log.info("exception : {}",e);
 		}
-		else {
-			return new ResponseEntity<List<Task>>(taskList,HttpStatus.NO_CONTENT);
-		}
+		return null;
 	}
 	
 	@GetMapping("/creationDate")
 	public ResponseEntity<List<Task>> getTaskByCreationDate(@RequestBody Task task){
-		List<Task> taskList = taskService.getTaskByCreationDate(task.getCreationDate());
-		log.info("taskList :{} ",taskList);
-		if(!taskList.isEmpty()) {
-			return new ResponseEntity<List<Task>>(taskList,HttpStatus.OK);
+		try {
+			List<Task> taskList = taskService.getTaskByCreationDate(task.getCreationDate());
+			log.info("taskList :{} ",taskList);
+			if(!taskList.isEmpty()) {
+				return new ResponseEntity<List<Task>>(taskList,HttpStatus.OK);
+			}
+			else {
+				return new ResponseEntity<List<Task>>(taskList,HttpStatus.NO_CONTENT);
+			}
+		} catch (Exception e) {
+			log.info("exception : {}",e);
 		}
-		else {
-			return new ResponseEntity<List<Task>>(taskList,HttpStatus.NO_CONTENT);
-		}
+	return null;	
 	}
 	
 
 	@GetMapping("/completionDate")
 	public ResponseEntity<List<Task>> getTaskByCompletionDate(@RequestBody Task task){
-		List<Task> taskList = taskService.getTaskByCompletionDate(task.getCompletionDate());
-		log.info("taskList:{} ",taskList);
-		if(!taskList.isEmpty()) {
-			return new ResponseEntity<List<Task>>(taskList,HttpStatus.OK);
+		try {
+			List<Task> taskList = taskService.getTaskByCompletionDate(task.getCompletionDate());
+			log.info("taskList:{} ",taskList);
+			if(!taskList.isEmpty()) {
+				return new ResponseEntity<List<Task>>(taskList,HttpStatus.OK);
+			}
+			else {
+				return new ResponseEntity<List<Task>>(taskList,HttpStatus.NO_CONTENT);
+			}
+		} catch (Exception e) {
+			log.info("exception : {}",e);
 		}
-		else {
-			return new ResponseEntity<List<Task>>(taskList,HttpStatus.NO_CONTENT);
-		}
+		return null;
 	}
 	
 }
